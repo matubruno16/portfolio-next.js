bb8932a92a5cbc377a1601c35e2429a8a21070a3
matubruno16 <matubruno16@gmail.com>
2024-10-22 15:25:04 -0300

    Update Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    "Matias Bruno - Desarrollador Frontend Web | HTML, CSS, JavaScript, React.JS y Next.JS",
+    "Matias Bruno - Desarrollador Web con Wordpress, React.JS y Next.JS",
   description:
-    "¡Hola! Soy Matias Bruno, un Desarrollador Frontend Junior apasionado por crear sitios web cautivadores y funcionales. Con habilidades sólidas en HTML, CSS y JavaScript, impulso la presencia en línea de tu negocio. Descubre cómo puedo mejorar tu presencia web hoy.",
+    "Desarrollador web especializado en WordPress, React.js y Next.js. Crea sitios web rápidos, modernos y personalizados para mejorar tu presencia online.",
   keywords:
     "Matias Bruno, Desarrollador Web, Desarrollador Frontend, HTML, CSS, JavaScript, React.JS, Next.JS, Sitios Web, Agencias Web, Desarrollo Web, Diseño Web",
   authors: [{ name: "Matias Bruno" }],
